73eb40809fb5471a9b7dc8ce3a7861afc86c2b0a
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-11-11 16:49:59 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,12 +1,24 @@
+import { data } from 'jquery';
 import React from 'react';
-import { actors } from '../data';
+import { actors, movies } from '../data';
 
 const Actors = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="actor">
+    <h1>Actors Page</h1>
+    {actors.map((a, idx) => (
+      <div key={idx}>
+        <h3>Name: {a.name}</h3>
+        <p>Movies:</p>
+    <ul>
+      {a.movies.map((m, idx) => (
+        <li key={idx}>{m}</li>
+        ))}
+    </ul>
     </div>
-  );
+  ))};
+  </div>
+  )
 };
 
 export default Actors;
